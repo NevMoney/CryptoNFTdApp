986bd6994f87c005f4c65bb9d728ef15b3582006
Nev <67838965+NevMoney@users.noreply.github.com>
2020-08-13 20:16:19 -0400

    adding cat DNA components

--- client/assets/js/catFactory.js
+++ client/assets/js/catFactory.js
@@ -45,11 +45,21 @@ function pawsColor(color, code) {
 //###################################################
 function eyeVariation(num) {
   $("#dnashape").html(num);
-  switch (num) {
+  switch (
+    num //switch statement is like a simple "if" statement... if 1 do this, if 2 do this, if 3 do that...
+  ) {
     case 1:
+      normalEyes(); //calling norrmalEyes function --> see line 72 (for now... scroll down)
+      $("#eyeName").html("Basic"); //set the badge on slider
+      break; //always have the have break at the end of the switch statements
+    case 2:
       normalEyes();
-      $("#eyeName").html("Basic");
+      $("#eyeName").html("Chill");
+      return eyesType1();
       break;
+    //could create "default" state by stating
+    //default:
+    //put your default here (don't need break for default)
   }
 }
 
@@ -64,7 +74,13 @@ function decorationVariation(num) {
 }
 
 async function normalEyes() {
-  await $(".cat__eye").find("span").css("border", "none");
+  //used in function eyeVariation --> see line 46
+  await $(".eyes").find("span").css("border", "none"); //find class you want to change, find all spans within that and then change css; border none is standard
+}
+
+//after normalEyes, we have to set different types:
+function eyesType1() {
+  $(".eyes").find("span").css("border-top", "15px solid"); //in parenthesis set the eye type you want --> currently using Filip's code
 }
 
 async function normaldecoration() {
