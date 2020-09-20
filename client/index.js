e71950d9e1529b0b0f4d7a9789a2daa6298889a7
Nev <67838965+NevMoney@users.noreply.github.com>
2020-09-20 19:16:43 -0400

    indes.js and abi.js function
    changed abi.js into one creating 2: one for kittyContrac and one for KittyMarketplace
    
    created async functions to allow for purchase/sale and pulling cat info

--- client/index.js
+++ client/index.js
@@ -4,8 +4,10 @@ var web3 = new Web3(Web3.givenProvider);
 
 //need to identify a few variables + use contract address from Ganache every time deployed
 var instance;
+var marketplaceInstance;
 var user;
 var contractAddress = "0xF49848D96B723263fd77bEdf94006c7E91aF47Ca";
+var marketplaceAddress = "0x1220d93278Ee8dE5F82b02B6Fe01318B9BAbac9E";
 
 
 
@@ -15,7 +17,8 @@ var contractAddress = "0xF49848D96B723263fd77bEdf94006c7E91aF47Ca";
 //find abi under the contract in build folder, then create new file called abi.js and past the object
 $(document).ready(function(){
     window.ethereum.enable().then(function(accounts){
-        instance = new web3.eth.Contract(abi, contractAddress, {from: accounts[0]});
+        instance = new web3.eth.Contract(abi.kittyContract, contractAddress, {from: accounts[0]});
+        marketplaceInstance = new web3.eth.Contract(abi.marketplace, marketplaceAddress, {from: accounts[0]});
         user = accounts[0];
 
         console.log(accounts);
@@ -35,11 +38,27 @@ $(document).ready(function(){
                                     + ", momId: " + momId 
                                     + ", dadId: " + dadId 
                                     + ", genes: " + genes)
-        })
+        });
         .on("error", console.error);
 
+        marketplaceInstance.events.MarketTransaction().on("data", (event) => {
+            console.log(event);
+            var eventType = event.returnValues["TxType"].toString();
+            var tokenId = events.returnValues["tokenId"];
+            if (eventType == "Kitty purchased"){
+                alert_msg("Successful Kitty purchase! You are the new owner of this Kitty with Token ID: " + tokenId);
+            }
+            if(eventType == "Offer created"){
+                alert_msg("Success! Offer set for Kitty ID: " + tokenId)
+            //see week 9 day 5 video 3 for additional stuff you can add   
+            }
+            if(eventType == "Offer removed"){
+                alert_msg("Successfully removed offer to sell Kitty ID: " + tokenId)
+                //again see W9 D5 Vid3 for add stuff
+            }
+        });
+        .on("error", console.error);
     });
-   
 });
 
 function createKitty(){
@@ -53,8 +72,7 @@ function createKitty(){
 }
 
 
-//displaying kitty based on user address
-//once page is loaded you run getKitties function
+//displaying kitty based on user address; once page is loaded you run getKitties function
 async function getKitties(){
 
     var arrayId;
@@ -73,5 +91,120 @@ async function getKitties(){
         console.log("kitty info:", kitty);
         appendCat(kitty.genes, i);
     }
-   
+}
+
+//send user to give permission to marketplace to run as operator
+async function initMarketplace() {
+    var isMarketplaceOperator = await instance.methods.isApprovedForAll(user, marketplaceAddress).call();
+
+    if(isMarketplaceOperator) {
+        getInventory();
+    }
+    else {
+        await instance.methods.setApprovalForAll(marketplaceAddress, true).send().on("receipt", function(receipt){
+            console.log("operator approval set");
+            getInventory();
+        });
+    }
+}
+
+//get the inventory of cats available for sale
+async function getInventory() {
+    var arrayId = await marketplaceInstance.methods.getAllTokenOnSale().call();
+    console.log("getInventory array: ", arrayId);
+    for(i = 0; i < arrayId.length; i++){
+        if(arrayId[i] != 0){
+            appendKitty(arrayId[i]);
+        }
+    }
+}
+
+//kitties for breeding
+async function breedCats(gender) {
+    var arrayId = await instance.methods.getKittyByOwner(user).call();
+    for(i = 0; i < arrayId.length; i++) {
+        appendBreed(arrayId[i], gender);
+    }
+}
+
+//append cat to breed selection
+async function appendBreed(id, gender) {
+    var kitty = await instance.methods.getKitty(id).call();
+    breedAppend(kitty[0], id, kitty.generation, gender);
+}
+
+//appending cat to breed select
+async function breed(dadId, momId) {
+    try {
+        var newKitty = await instance.methods.breed(dadId, momId).send();
+        console.log("newKitty: ", newKitty);
+        setTimeout(() => {
+            go_to(".catalog")
+        }, 2000)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+//appending cats for catalog
+async function appendKitty(id) {
+    var kitty = await instance.methods.getKitty(id).call();
+    appendCat(kitty[0], id, kitty["generation"]);
+}
+
+async function singleKitty() {
+    var id = get_variables().catId;
+    var kitty = await instance.methods.getKitty(id).call();
+    renderSingleCat(kitty[0], id, kitty["generation"]);
+}
+
+async function catOwnership(id) {
+    var address = await instance.methods.ownerOf(id),call();
+    if (address.toLowerCase() == user.toLowerCase()) {
+        return true;
+    }
+    return false;
+}
+
+async function sellCat(id) {
+    var price = $("#catPrice").val();
+    var amount = web3.utils.toWei(price, "ether");
+    try {
+        await marketplaceInstance.methods.setOffer(amount, id).send();
+    }
+    catch (err) {
+        console.log(err);
+    } 
+}
+
+async function buyKitten(id, price) {
+    var amount = web3.utils.toWei(price, "ether");
+    try {
+        await marketplaceInstance.methods.buyKitty(id).send({value: amount});
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function checkOffer(id) {
+    let res;
+
+    try {
+        res = await marketplaceInstance.methods.getOffer(id).call();
+        var price = res["price"];
+        var seller = res["seller"];
+        var onsale = false;
+
+        if (price > 0) {
+            onsale = true;
+        }
+        price = web3.utils.fromWei(price, "ether");
+        var offer = {seller: seller, price: price, onsale: onsale}
+        return offer;
+    }
+    catch (err) {
+        console.log(err);
+        return;
+    }
 }
