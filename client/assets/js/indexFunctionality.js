a2d8810a26b317bd021cae74447c6ad354a0ec6f
Nev <67838965+NevMoney@users.noreply.github.com>
2020-09-25 08:53:32 -0400

    minor changes to indexfunctionality.js
    forgot to push before calling it a day

--- client/assets/js/indexFunctionality.js
+++ client/assets/js/indexFunctionality.js
@@ -150,14 +150,14 @@ $("#breedBtn").click(function(){
   $(".breed").show();
 });
 
-$("#momDiv").click(function() {
-  $("#kittyPopup").show();
-});
+// $("#momDiv").click(function() {
+//   $("#kittyPopup").show();
+// });
 
-$("#dadDiv").click(function() {
-  $("#kittyPopup").show();
-});
+// $("#dadDiv").click(function() {
+//   $("#kittyPopup").show();
+// });
 
-function togglePopup() {
-  document.getElementById("kittyPopup").classList.toggle("active");
-}
+// function togglePopup() {
+//   document.getElementById("kittyPopup").classList.toggle("active");
+// }
